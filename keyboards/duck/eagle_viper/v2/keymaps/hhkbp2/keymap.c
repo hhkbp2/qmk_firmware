3909916cd93dac7f00093d3401f73cbe1b7c34a3
Dylan.Wen <hhkbp2@gmail.com>
2019-01-09 00:31:02 +0800

    revise viper keymap

--- keyboards/duck/eagle_viper/v2/keymaps/hhkbp2/keymap.c
+++ keyboards/duck/eagle_viper/v2/keymaps/hhkbp2/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_viper( \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, \
     KC_CAPS, RGB_TOG, RGB_MOD, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,          KC_DEL,  \
-    _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,                   KC_ENT,  \
+    _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,                   _______, \
     _______,          _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN,          KC_DOWN, _______, _______, \
              _______, _______,                                     _______,                                     _______, _______           \
   ),
