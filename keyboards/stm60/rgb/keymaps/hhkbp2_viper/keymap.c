9ffa69b3cb0a4897879306beb3b7f7acbef8786e
Dylan.Wen <hhkbp2@gmail.com>
2019-06-15 20:25:37 +0800

    simplify keymap

--- keyboards/stm60/rgb/keymaps/hhkbp2_viper/keymap.c
+++ keyboards/stm60/rgb/keymaps/hhkbp2_viper/keymap.c
@@ -13,8 +13,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = KEYMAP_hhkb(  \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,  \
-    KC_CAPS, RGBM_TOG,RGBM_MOD,RGBM_HUI,RGBM_HUD,RGBM_SAI,RGBM_SAD,RGBM_VAI,RGBM_VAD,_______, _______, KC_UP,   _______,          KC_DEL,   \
-    _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_PGUP, KC_LEFT, KC_RGHT,                   _______,  \
+    KC_CAPS, RGBM_TOG,RGBM_MOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, KC_UP,   _______,          KC_DEL, \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,                   _______, \
     _______,          _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN,          _______, _______,  \
              _______, _______,                            _______,                            _______, _______                               \
   ),
