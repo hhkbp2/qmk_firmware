3309c8511cacb2c85a10913d4f3879eada2d10b8
Dylan.Wen <hhkbp2@gmail.com>
2019-09-17 18:05:42 +0800

    enable rgb matrix for my viper

--- keyboards/stm60/dp60/keymaps/hhkbp2_viper/keymap.c
+++ keyboards/stm60/dp60/keymaps/hhkbp2_viper/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_60_hhkb(
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_PSCR,
-    KC_CAPS,  RGB_TOG,  RGB_MOD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,            KC_DEL,
+    KC_CAPS,  RGB_TOG,  RGB_MOD,  RGBM_TOG, RGBM_MOD, _______,  _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,            KC_DEL,
     _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGUP,  KC_LEFT,  KC_RGHT,                      _______,
     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_END,   KC_PGDN,  KC_DOWN,            _______,  _______,
               _______,  _______,                                _______,                                _______,  _______
--- keyboards/stm60/dp60/keymaps/hhkbp2_viper/rules.mk
+++ keyboards/stm60/dp60/keymaps/hhkbp2_viper/rules.mk
@@ -1,6 +1,6 @@
 # Enable backlight but not RGB Matrix for viper
 RGBLIGHT_ENABLE = yes       # Use RGB bottom light
-RGB_MATRIX_ENABLE = no      # Disable RGB matrix
+RGB_MATRIX_ENABLE = yes     # Enable RGB matrix
 
 # Disable dynamic keymap because I usually uses qmk toolbox to flash keymaps
 DYNAMIC_KEYMAP_ENABLE = no
